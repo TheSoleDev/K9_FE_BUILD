9899285321ab54a5dae50e1fe88ca8ba29fc58c2
juldio <garciajuliusb@yahoo.com>
2019-01-21 21:30:02 +0800

    INITIAL BUILD

---
+++ precache-manifest.3a07d85b28c4bc0bcf5515101f9b4f9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f16e7674363d45d536e9",
+    "url": "/static/js/main.f16e7674.chunk.js"
+  },
+  {
+    "revision": "eb881f339a03770af309",
+    "url": "/static/js/1.eb881f33.chunk.js"
+  },
+  {
+    "revision": "f16e7674363d45d536e9",
+    "url": "/static/css/main.24088eae.chunk.css"
+  },
+  {
+    "revision": "eb881f339a03770af309",
+    "url": "/static/css/1.ffa927ee.chunk.css"
+  },
+  {
+    "revision": "fe0de56a310454bd162089f6837fbd50",
+    "url": "/index.html"
+  }
+];
